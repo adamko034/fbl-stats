655e112ae57ca7e3f8fc963f2c020c636190432c
Adam Kotas <adamko034@outlook.com>
2020-12-28 20:48:40 +0100

    fixes: prediction yellow if any unpublished source, unavailable link when no url

--- src/app/layout/content/components/players-table-container/services/player-attendance-prediction.service.ts
+++ src/app/layout/content/components/players-table-container/services/player-attendance-prediction.service.ts
@@ -9,14 +9,16 @@ export class PlayerAttendancePredictionService {
       return PlayerAttendancePrediction.UnknownYet;
     }
 
-    const availablePredictions = predictions.filter((p) => p.attendance !== -1);
+    // const availablePredictions = predictions.filter((p) => p.attendance !== -1);
 
-    const willPlay = availablePredictions.every((p) => p.attendance === 1);
+    // const willPlay = availablePredictions.every((p) => p.attendance === 1);
+    const willPlay = predictions.every((p) => p.attendance === 1);
     if (willPlay) {
       return PlayerAttendancePrediction.WillPlay;
     }
 
-    const wontPlay = availablePredictions.every((p) => p.attendance === 0);
+    // const wontPlay = availablePredictions.every((p) => p.attendance === 0);
+    const wontPlay = predictions.every((p) => p.attendance === 0);
     if (wontPlay || predictions.length === 0) {
       return PlayerAttendancePrediction.WillNotPlay;
     }
