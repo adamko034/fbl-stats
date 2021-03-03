ee276f69b576efedc5aee27bf90b892a1864f263
adamko <adamko034@outlook.com>
2021-03-03 22:29:44 +0100

    leaders matchdays timeline

--- src/app/modules/leaders/leaders-content/leaders-navigation/leaders-navigation.component.ts
+++ src/app/modules/leaders/leaders-content/leaders-navigation/leaders-navigation.component.ts
@@ -1,8 +1,12 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+import { combineLatest, Observable } from 'rxjs';
+import { distinctUntilChanged, map } from 'rxjs/operators';
+import { ArrayStream } from 'src/app/services/array-stream.service';
+import { ScreenSize, ScreenSizeService } from 'src/app/services/screen-size.service';
 
+@UntilDestroy()
 @Component({
   selector: 'app-leaders-navigation',
   templateUrl: './leaders-navigation.component.html',
@@ -10,11 +14,70 @@ import { map } from 'rxjs/operators';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LeadersNavigationComponent implements OnInit {
-  public matchdays$: Observable<number[]>;
+  private matchdays: number[];
+  private matchdaysCountToDisplay = 5;
 
-  constructor(private route: ActivatedRoute) {}
+  public matchdayText = 'Matchday';
+  public matchdaysToDisplay: number[];
+  public isMax: boolean;
+  public isMin: boolean;
+
+  constructor(
+    private route: ActivatedRoute,
+    private screenSizeService: ScreenSizeService,
+    private changeDetection: ChangeDetectorRef
+  ) {}
 
   public ngOnInit(): void {
-    this.matchdays$ = this.route.data.pipe(map((data) => data.matchdaysNumbers));
+    combineLatest([this.route.data, this.screenSizeService.onResize()])
+      .pipe(
+        distinctUntilChanged(),
+        map(([data, screenSize]) => ({ matchdays: data.matchdaysNumbers, screenSize })),
+        untilDestroyed(this)
+      )
+      .subscribe((res: { matchdays: number[]; screenSize: ScreenSize }) => {
+        this.matchdays = res.matchdays.sort((a, b) => b - a);
+        this.matchdayText = 'Matchday';
+
+        if (res.screenSize >= ScreenSize.MD) {
+          this.matchdaysCountToDisplay = 5;
+        }
+
+        if (res.screenSize < ScreenSize.MD) {
+          this.matchdaysCountToDisplay = 5;
+          this.matchdayText = 'MD';
+        }
+
+        if (res.screenSize < ScreenSize.SM) {
+          this.matchdaysCountToDisplay = 2;
+        }
+
+        this.matchdaysToDisplay = this.matchdays.slice(0, this.matchdaysCountToDisplay);
+        this.calculateMinAndMax();
+
+        this.changeDetection.detectChanges();
+      });
+  }
+
+  public nextMatchdays() {
+    const nextMatchday = Math.max(...this.matchdaysToDisplay) + 1;
+    this.matchdaysToDisplay = this.matchdays.filter(
+      (x) => x <= nextMatchday && x > nextMatchday - this.matchdaysCountToDisplay
+    );
+
+    this.calculateMinAndMax();
+  }
+
+  public previousMatchdays() {
+    const previous = this.matchdaysToDisplay[1];
+    this.matchdaysToDisplay = this.matchdays.filter(
+      (x) => x <= previous && x > previous - this.matchdaysCountToDisplay
+    );
+    this.calculateMinAndMax();
+  }
+
+  private calculateMinAndMax() {
+    this.isMax = Math.max(...this.matchdays) === Math.max(...this.matchdaysToDisplay);
+    this.isMin = Math.min(...this.matchdays) === Math.min(...this.matchdaysToDisplay);
   }
 }
