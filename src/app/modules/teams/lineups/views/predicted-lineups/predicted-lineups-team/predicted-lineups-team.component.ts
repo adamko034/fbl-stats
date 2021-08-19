c5e569eb57b3f23d1d9ef70aace19f130db0f24c
adamko <adamko034@outlook.com>
2021-08-19 14:58:41 +0200

    lineups details - finished

--- src/app/modules/teams/lineups/views/predicted-lineups/predicted-lineups-team/predicted-lineups-team.component.ts
+++ src/app/modules/teams/lineups/views/predicted-lineups/predicted-lineups-team/predicted-lineups-team.component.ts
@@ -1,7 +1,9 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { MatCheckboxChange } from '@angular/material/checkbox';
 import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, combineLatest, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { PlayerAttendancePrediction } from 'src/app/modules/core/players/models/player-attendance-prediction.enum';
 import { TeamPredictedLineups } from 'src/app/modules/teams/lineups/store/models/team-predicted-lineups.model';
 import { Player } from 'src/app/store/players/models/player.model';
 
@@ -13,13 +15,48 @@ import { Player } from 'src/app/store/players/models/player.model';
 })
 export class PredictedLineupsTeamComponent implements OnInit {
   public teamPredictions$: Observable<TeamPredictedLineups>;
+  public absences$: Observable<Player[]>;
   public players$: Observable<Player[]>;
-  public defenders$: Observable<Player>;
+
+  private hideBenchedChange$ = new BehaviorSubject<boolean>(false);
 
   constructor(private route: ActivatedRoute) {}
 
   public ngOnInit(): void {
     this.teamPredictions$ = this.route.data.pipe(map((data) => data.teamPredictions));
-    this.players$ = this.route.data.pipe(map((data) => data.players));
+    this.absences$ = this.route.data.pipe(
+      map((data) => {
+        var players: Player[] = data.players;
+
+        return players.filter((p) => p.attendance === 0);
+      })
+    );
+    this.players$ = combineLatest([this.hideBenchedChange$, this.route.data]).pipe(
+      map(([hideBenched, data]) => {
+        var players: Player[] = data.players;
+
+        if (hideBenched) {
+          players = players.filter(
+            (p) => !p.nextGame.lineupPredictions.every((l) => l.attendance === PlayerAttendancePrediction.WillNotPlay)
+          );
+        }
+
+        return players;
+      })
+    );
+  }
+
+  public getPrediction(player: Player, source: string): PlayerAttendancePrediction {
+    var predictions = player.nextGame.lineupPredictions.filter((l) => l.source === source);
+
+    if (!predictions || predictions.length === 0) {
+      return PlayerAttendancePrediction.UnknownYet;
+    }
+
+    return predictions[0].attendance;
+  }
+
+  public onHideBenchedChange(matChange: MatCheckboxChange): void {
+    this.hideBenchedChange$.next(matChange.checked);
   }
 }
